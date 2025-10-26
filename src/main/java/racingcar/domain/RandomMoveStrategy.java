99981fe9696ed057de5d2e6aa5e81417a8af553a
choiyoung69 <choiyoung609@gmail.com>
2025-10-27 03:55:59 +0900

    feat(RandomMoveStrategy) : 랜덤하게 전진을 결정하는 전략 클래스 생성

---
+++ src/main/java/racingcar/domain/RandomMoveStrategy.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomMoveStrategy implements MoveStrategy {
+    private static final int RANDOM_START_NUMBER = 0;
+    private static final int RANDOM_END_NUMBER = 9;
+    private static final int MOVE_THRESHOLD = 4;
+
+    @Override
+    public boolean isAllowedToAdvance() {
+        return Randoms.pickNumberInRange(RANDOM_START_NUMBER, RANDOM_END_NUMBER) >= MOVE_THRESHOLD;
+    }
+}
