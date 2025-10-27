b58c29375dfaebeb772f4fea3c1166bcabc3ac5f
choiyoung69 <choiyoung609@gmail.com>
2025-10-27 15:18:20 +0900

    feat(OutputView): 우승자를 추출하는 메서드 생성

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,12 +1,16 @@
 package racingcar.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.domain.Car;
 import racingcar.domain.Cars;
 
 public class OutputView {
     private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_ANNOUNCEMENT_MESSAGE = "최종 우승자 : ";
     private static final String NAME_RESULT_SEPARATOR = " : ";
     private static final String POSITION_SYMBOL = "-";
+    private static final String WINNER_DELIMITER = ", ";
 
 
     public static void printExecutionResultMessage() {
@@ -22,5 +26,12 @@ public class OutputView {
 
     public static String getPositionString(int carDistance) {
         return POSITION_SYMBOL.repeat(carDistance);
+    }
+
+    public static void printWinner(List<Car> winners) {
+        String winnerNames = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(WINNER_DELIMITER));
+        System.out.println(WINNER_ANNOUNCEMENT_MESSAGE + winnerNames);
     }
 }
