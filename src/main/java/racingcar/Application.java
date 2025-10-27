b7a6feaa01a3023ea3b73f4eead0ef76f748bec5
choiyoung69 <choiyoung609@gmail.com>
2025-10-27 18:34:16 +0900

    refactor: CommaParser를 싱글톤 객체로 만들기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,7 +7,7 @@ import racingcar.domain.Parser.Parser;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Parser parser = new CommaParser();
+        Parser parser = CommaParser.getInstance();
         RacingGameController racingGameController = new RacingGameController(parser);
         racingGameController.gameStart();
     }
--- src/main/java/racingcar/domain/Parser/CommaParser.java
+++ src/main/java/racingcar/domain/Parser/CommaParser.java
@@ -5,6 +5,13 @@ import java.util.List;
 
 public class CommaParser implements Parser {
     private static final String COMMA_DELIMITER = ",";
+    private static final CommaParser INSTANCE = new CommaParser();
+
+    private CommaParser() {}
+
+    public static CommaParser getInstance() {
+        return INSTANCE;
+    }
 
     @Override
     public List<String> parse(String carNames) {
