82cd8c06b99ed8f94760688607d6475032281535
choiyoung69 <choiyoung609@gmail.com>
2025-10-27 13:47:00 +0900

    feat(Game): 게임을 관리할 Game 객체 생성

---
+++ src/main/java/racingcar/domain/Game.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+public class Game {
+    private final Cars cars;
+    private final int totalRounds;
+    private int currentRound;
+
+    private Game(Cars cars, int totalRounds) {
+        validatePositiveRounds(totalRounds);
+        this.cars = cars;
+        this.totalRounds = totalRounds;
+        this.currentRound = 0;
+    }
+
+    public static Game from(Cars cars, int totalRounds) {
+        return new Game(cars, totalRounds);
+    }
+
+    private void validatePositiveRounds(int totalRounds) {
+        if(totalRounds <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 1 이상이여야 합니다.");
+        }
+    }
+}
