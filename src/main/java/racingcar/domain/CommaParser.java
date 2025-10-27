6d2118866f8ab56bafe64d2caa9d445a5fff056f
choiyoung69 <choiyoung609@gmail.com>
2025-10-27 14:31:44 +0900

    feat(CommaParser): comma로 문자열을 구분하는 Parser 생성

---
+++ src/main/java/racingcar/domain/CommaParser.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CommaParser implements Parser{
+    private static final String COMMA_DELIMITER = ",";
+
+    @Override
+    public List<String> parse(String carNames) {
+        return Arrays.stream(carNames.split(COMMA_DELIMITER))
+                .toList();
+    }
+}
